1a0f6791fe013baeca8b1b2f7fd6aa9f9af649df
Alexey <aav8402@mail.ru>
2022-09-17 16:42:03 +0300

    Задача B*B=A

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа, которая на вход принимает 2 числа и проверяет, 
+// является ли первое число квадратом второго
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int result = B * B;
+Console.WriteLine("Результат " + result);
+
+if (result == A)
+{
+Console.WriteLine($"A квадрат от B");
+}
+else
+{
+    Console.WriteLine($"Нет");
+}
